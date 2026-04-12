d6430b6972828f4a96334d9fcb3b791a37a8e49f
Nikita D <dnawastaken@gmail.com>
2026-04-12 16:51:14 -0700

    initial commit

---
+++ cli/scripts/download_models.sh
@@ -0,0 +1,2 @@
+mkdir -p checkpoints
+curl -L -o "checkpoints/kokoro-v1.0.onnx" "https://github.com/thewh1teagle/kokoro-onnx/releases/download/model-files-v1.0/kokoro-v1.0.onnx"
---
+++ cli/scripts/download_voices.sh
@@ -0,0 +1,2 @@
+mkdir data
+curl -L -o "data/voices-v1.0.bin" "https://github.com/thewh1teagle/kokoro-onnx/releases/download/model-files-v1.0/voices-v1.0.bin"
