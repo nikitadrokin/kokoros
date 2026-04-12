ac4601e458a4745ebe1c9be936bb1ca94d8a89d2
Nikita D <dnawastaken@gmail.com>
2026-04-12 16:57:10 -0700

    app works

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -7,7 +7,15 @@ import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 const config = defineConfig({
+  base: './',
+  clearScreen: false,
+  envPrefix: ['VITE_', 'TAURI_ENV_*'],
   resolve: { tsconfigPaths: true },
+  server: {
+    port: 3000,
+    strictPort: true,
+    watch: { ignored: ['**/src-tauri/**'] },
+  },
   plugins: [
     devtools(),
     tailwindcss(),
